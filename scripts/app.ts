644f6da61efc27b1ae9e96e9b5c0323aea4f6297
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2019-02-12 15:42:37 -0700

    Pulling the latest block from web3

--- scripts/app.ts
+++ scripts/app.ts
@@ -1,13 +1,21 @@
-import express = require('express');
-import bodyParser = require('body-parser');
-import cors = require('cors');
+import express = require('express')
+import bodyParser = require('body-parser')
+import cors = require('cors')
+import { Block } from 'web3/eth/types'
+const Web3 = require('web3')
+require('dotenv').config()
 
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
+const web3 = new Web3(process.env.GETH_URL)
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
 
 app.get('/', (request, response) => {
-  response.json('hello');
-});
+  web3.eth.getBlockNumber().then((blockNumber: Number) => {
+    web3.eth.getBlock(blockNumber).then((block: Block) => {
+      response.json(block)
+    })
+  })
+})
 
-app.listen(process.env.PORT || 3001);
+app.listen(process.env.PORT || 3001)
