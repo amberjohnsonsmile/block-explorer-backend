8a8a02ddeb4333e607a64694081421b5ec035bad
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2019-03-13 10:47:04 -0600

    Adding test for getting a block from web3

---
+++ test/web3-test.ts
@@ -0,0 +1,22 @@
+import { assert } from 'chai'
+require('dotenv').config()
+const Web3 = require('web3')
+
+describe('web3', function() {
+  it('can get a block from geth', async function() {
+    const web3 = new Web3(process.env.GETH_URL)
+    const block = await web3.eth.getBlock(6000000)
+
+    assert.isNotNull(block, 'A block should be returned from web3')
+    assert.equal(
+      block.number,
+      6000000,
+      'Block 6000000 should be returned from web3'
+    )
+    assert.equal(
+      block.hash,
+      '0xbe847be2bceb74e660daf96b3f0669d58f59dc9101715689a00ef864a5408f43',
+      'The block hash should be correct'
+    )
+  })
+})
